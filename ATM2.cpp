c7342b189412d56d1a13384b90cc2c408c159b0a
Human-ctrl-ai <dassuvra336@gmail.com>
2026-02-27 23:13:22 +0530

    Add files via upload

---
+++ ATM2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	for(int j=0; j<t; j++){
+	    int n, k;
+	    cin >> n >> k;
+	    int arr[n];
+	    for(int i=0; i<n; i++){
+	        cin >> arr[i];
+	    }
+	    
+	    for(int i=0; i<n; i++){
+	        if(k >= arr[i]){
+	            cout << 1;
+	            k = k - arr[i];
+	        } else{
+	            cout << 0;
+	        }
+	    }
+	    cout << endl;
+	    
+	}
+	return 0;
+}
